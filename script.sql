ab4525d16857ac67885c8b446639e22eb44a621e
dreacotes <dreacotes@gmail.com>
2020-10-07 12:02:10 -0500

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,5 +1,8 @@
-/* ****************************************************************
-    TEST SQL SCRIPT.
-
-    SQL > 'SELECT STATEMENT HERE' 
-    *************************************************************** */
+set serveroutput on
+set echo off
+SET HEADING OFF 
+spool c:\temp\scriptsql.log
+select sysdate from dual;
+select '------->PRUEBAS' title from dual;
+SET SERVEROUTPUT OFF
+spool off
